d4dc281af30bd569ad357f604ea9b3636b6858ea
Janek1010 <janekdomozych99@gmail.com>
2024-02-15 11:22:05 +0100

    3 done

---
+++ 3-TreasuryIsland/main.py
@@ -0,0 +1,53 @@
+print('''
+*******************************************************************************
+                                                                   ,-,
+                                                             _.-=;~ /_
+                                                          _-~   '     ;.
+                                                      _.-~     '   .-~-~`-._
+                                                _.--~~:.             --.____88
+                              ____.........--~~~. .' .  .        _..-------~~
+                     _..--~~~~               .' .'             ,'
+                 _.-~                        .       .     ` ,'
+               .'                                    :.    ./
+             .:     ,/          `                   ::.   ,'
+           .:'     ,(            ;.                ::. ,-'
+          .'     ./'.`.     . . /:::._______.... _/:.o/
+         /     ./'. . .)  . _.,'               `88;?88|
+       ,'  . .,/'._,-~ /_.o8P'                  88P ?8b
+    _,'' . .,/',-~    d888P'                    88'  88|
+ _.'~  . .,:oP'        ?88b              _..--- 88.--'8b.--..__
+:     ...' 88o __,------.88o ...__..._.=~- .    `~~   `~~      ~-._ Seal _.
+`.;;;:='    ~~            ~~~                ~-    -       -   -
+
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+print("Welcome to Treasure Island.Your mission is to find the treasure.")
+choice = input("left or right?").lower()
+if choice == "left":
+    choice = input("swim or wait?").lower()
+
+    if choice == "wait":
+        choice = input("Which door?").lower()
+        if choice == "red":
+            print("Burned by fire. Game Over.")
+        elif choice == "blue":
+            print("Eaten by beasts.Game Over.")
+        elif choice == "yellow":
+            print("You Win.")
+        else:
+            print("Game Over.")
+
+    else:
+        print("Attacked by trout. Game Over.")
+
+
+
+else:
+    print("Fall into a hole.Game Over.")
